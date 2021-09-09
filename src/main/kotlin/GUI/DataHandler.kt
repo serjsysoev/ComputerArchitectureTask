2eaa1fcd15c381ce08709e145e4adc175a9e7b53
kirvader <kirkondratiuk@gmail.com>
2021-09-09 23:50:46 +0300

    added link for back end front

--- src/main/kotlin/GUI/DataHandler.kt
+++ src/main/kotlin/GUI/DataHandler.kt
@@ -1,5 +1,6 @@
 package GUI
 
+import DecimalConverter
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
@@ -29,6 +30,7 @@ class DataHandler {
     Основание -2
     Симметричная
      */
+    val decimalConverter = DecimalConverter(8)
 
     val decimal_State: MutableState<String> = mutableStateOf("")
     val noSign_State: MutableState<String> = mutableStateOf("")
@@ -41,7 +43,31 @@ class DataHandler {
     val baseMinus2_State: MutableState<String> = mutableStateOf("")
     val symmetric_State: MutableState<String> = mutableStateOf("")
 
+    fun
+
     fun updateDataBy(codec: EncodingType) {
-        // TODO() update with codec as main
+        val decimalUpdateNumber = when(codec) {
+            EncodingType.Decimal -> decimal_State.value.toIntOrNull()
+            EncodingType.NoSign -> TODO()
+            EncodingType.BitForSign -> TODO()
+            EncodingType.Skip128 -> TODO()
+            EncodingType.Skip127 -> TODO()
+            EncodingType.SupplementTo2 -> TODO()
+            EncodingType.SupplementTo1 -> TODO()
+            EncodingType.Alternation -> TODO()
+            EncodingType.BaseMinus2 -> TODO()
+            EncodingType.Symmetric -> TODO()
+        } ?: return
+        decimal_State.value = decimalUpdateNumber.toString()
+        noSign_State.value = "0b${decimalConverter.toUnsigned(decimalUpdateNumber)}"
+        noSign_State.value = "0b${decimalConverter.toUnsigned(decimalUpdateNumber)}"
+        bitForSign_State.value = "0b${decimalConverter.toSignedWithSignBit(decimalUpdateNumber)}"
+        skip128_State.value = "0b${decimalConverter.toSignedWithShift128(decimalUpdateNumber)}"
+        skip127_State.value = "0b${decimalConverter.toSignedWithShift127(decimalUpdateNumber)}"
+        supplementTo2_State.value = "0b${decimalConverter.toSignedTwosComplement(decimalUpdateNumber)}"
+        supplementTo1_State.value = "0b${decimalConverter.toSignedOnesComplement(decimalUpdateNumber)}"
+        alternation_State.value = "0b${decimalConverter.toSignedAlternating(decimalUpdateNumber)}"
+        baseMinus2_State.value = "0b${decimalConverter.toUnsigned(decimalUpdateNumber)}"
+        symmetric_State.value = "0b${decimalConverter.toUnsigned(decimalUpdateNumber)}"
     }
 }

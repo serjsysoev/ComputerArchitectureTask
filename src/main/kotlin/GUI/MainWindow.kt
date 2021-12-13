6ac2e6bff4e2b539db4bbe6304e92d923eb600f6
Sergey Sysoev <48771451+serjsysoev@users.noreply.github.com>
2021-12-13 15:55:24 +0300

    Revert "added input for each base"
    This reverts commit 0b79e6f06eeb8352224b497a88376b8b891084dd.

--- src/main/kotlin/GUI/MainWindow.kt
+++ src/main/kotlin/GUI/MainWindow.kt
@@ -16,37 +16,29 @@ import androidx.compose.ui.window.application
 fun mainWindow() = application {
     Window(title = "String completer", size = IntSize(500, 900)) {
         val dataHandler = DataHandler()
-        val answer = mutableStateOf("")
+        val baseLabel = mutableStateOf("Binary")
 
         MaterialTheme {
             Column(modifier = Modifier.fillMaxSize()) {
-/*Text(baseLabel.value,
-    modifier = Modifier.padding(8.dp).clickable(
-        onClick = {
-            dataHandler.isBaseBinary.value = !dataHandler.isBaseBinary.value
-            baseLabel.value = if (dataHandler.isBaseBinary.value) "binary" else "hex"
-            dataHandler.updateDataBy(EncodingType.Decimal)
-        },
+                Text(baseLabel.value,
+                    modifier = Modifier.padding(8.dp).clickable(
+                        onClick = {
+                            dataHandler.isBaseBinary.value = !dataHandler.isBaseBinary.value
+                            baseLabel.value = if (dataHandler.isBaseBinary.value) "binary" else "hex"
+                            dataHandler.updateDataBy(EncodingType.Decimal)
+                        },
 
-    ))
-inputField(dataHandler.decimal_State, "десятичное", {dataHandler.updateDataBy(EncodingType.Decimal)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.noSign_State, "без знака", {dataHandler.updateDataBy(EncodingType.NoSign)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.bitForSign_State, "знак под бит", {dataHandler.updateDataBy(EncodingType.BitForSign)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.skip128_State, "сдвиг на 128", {dataHandler.updateDataBy(EncodingType.Skip128)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.skip127_State, "сдвиг на 127", {dataHandler.updateDataBy(EncodingType.Skip127)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.supplementTo2_State, "дополнить до 2", {dataHandler.updateDataBy(EncodingType.SupplementTo2)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.supplementTo1_State, "дополнить до 1", {dataHandler.updateDataBy(EncodingType.SupplementTo1)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.alternation_State, "с чередованием", {dataHandler.updateDataBy(EncodingType.Alternation)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.baseMinus2_State, "основание -2", {dataHandler.updateDataBy(EncodingType.BaseMinus2)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-inputField(dataHandler.symmetric_State, "симметричная", {dataHandler.updateDataBy(EncodingType.Symmetric)}, dataHandler::clearCells, dataHandler.isBaseBinary)
-}
-
- */
-
-                inputFieldSimple(dataHandler.decimal_State, "десятичное", {/* add function to get dataHandler.decimal_State.value and set answer.value*/})
-                Text(answer.value
-
-                )
+                    ))
+                inputField(dataHandler.decimal_State, "десятичное", {dataHandler.updateDataBy(EncodingType.Decimal)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.noSign_State, "без знака", {dataHandler.updateDataBy(EncodingType.NoSign)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.bitForSign_State, "знак под бит", {dataHandler.updateDataBy(EncodingType.BitForSign)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.skip128_State, "сдвиг на 128", {dataHandler.updateDataBy(EncodingType.Skip128)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.skip127_State, "сдвиг на 127", {dataHandler.updateDataBy(EncodingType.Skip127)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.supplementTo2_State, "дополнить до 2", {dataHandler.updateDataBy(EncodingType.SupplementTo2)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.supplementTo1_State, "дополнить до 1", {dataHandler.updateDataBy(EncodingType.SupplementTo1)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.alternation_State, "с чередованием", {dataHandler.updateDataBy(EncodingType.Alternation)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.baseMinus2_State, "основание -2", {dataHandler.updateDataBy(EncodingType.BaseMinus2)}, dataHandler::clearCells, dataHandler.isBaseBinary)
+                inputField(dataHandler.symmetric_State, "симметричная", {dataHandler.updateDataBy(EncodingType.Symmetric)}, dataHandler::clearCells, dataHandler.isBaseBinary)
             }
         }
     }
